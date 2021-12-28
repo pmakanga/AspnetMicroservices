8cafaceceeffd579ab3869dcfa102fb350b12a1b
partrick <pmakanga@gmail.com.com>
2021-12-28 22:13:33 +0300

    UpdateOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository orderRepositiry;
+        private readonly IMapper mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepositiry, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            this.orderRepositiry = orderRepositiry ?? throw new ArgumentNullException(nameof(orderRepositiry));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await orderRepositiry.GetByIdAsync(request.Id);
+            
+            if(orderToUpdate == null)
+            {
+                //throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await orderRepositiry.UpdateAsync(orderToUpdate);
+
+            return Unit.Value;
+        }
+    }
+}
